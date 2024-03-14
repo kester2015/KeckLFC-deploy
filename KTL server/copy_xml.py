17365ada2d1023f0db209baaae165d9c361efca6
YooJung-Kim <yjkim134340@gmail.com>
2024-03-13 18:05:08 -0700

    implement variable keyword read frequency

---
+++ KTL server/copy_xml.py
@@ -0,0 +1,24 @@
+import xml.etree.ElementTree as ET
+import subprocess
+'''
+This is a simple script to strip the <period> node in the keyword xml files
+and copy the xml file to irastrocombbuild
+'''
+
+# Parse the XML file
+tree = ET.parse('LFCm.xml.sin')
+root = tree.getroot()
+
+# Find all keyword elements
+keywords = root.findall('keyword')
+
+# Remove the <period> node from each keyword element
+for keyword in keywords:
+    period = keyword.find('period')
+    if period is not None:
+        keyword.remove(period)
+
+# Write the modified XML to a new file
+tree.write('LFC.xml.sin')
+subprocess.run(["scp", "LFC.xml.sin", "combbld@irastrocombbuild.keck.hawaii.edu:/kroot/src/kss/astrocomb/ktlxml/"])
+subprocess.run(["scp", "LFCm.xml.sin", "combbld@irastrocombbuild.keck.hawaii.edu:/kroot/src/kss/astrocomb/ktlxml/"])
